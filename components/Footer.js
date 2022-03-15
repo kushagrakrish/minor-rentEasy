1ca5f2fa86fc7aedab3a944f8c93d44d4931e50e
kushagrakrish <krishna619kk@gmail.com>
2022-03-16 00:36:26 +0530

    frontend complete

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-4 px-36 gap-y-10 py-14 bg-gray-100 text-gray-600'>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>About Section</h5>
+        <p>How RentEasy works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>RentEasy Plus</p>
+        <p>RentEasy Luxe</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>COMMUNITY</h5>
+        <p>How RentEasy works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>RentEasy Plus</p>
+        <p>RentEasy Luxe</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>HOST</h5>
+        <p>How RentEasy works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>RentEasy Plus</p>
+        <p>RentEasy Luxe</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>SUPPORT</h5>
+        <p>How RentEasy works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>RentEasy Plus</p>
+        <p>RentEasy Luxe</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import LargeCard from "../components/LargeCard";
+import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className=''>
       <Head>
@@ -30,7 +33,23 @@ export default function Home({ exploreData }) {
             ))}
           </div>
         </section>
+        <section>
+          <h2 className='text-4xl font-semibold mt-6 mb-6'>Live Anywhere</h2>
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {cardsData?.map(({ item, img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+
+        <LargeCard
+          img='https://links.papareact.com/4cj'
+          title='The Greatest Outdoors'
+          description='Wishlist curated by RentEasy'
+          buttonText='Get Inspired'
+        />
       </main>
+      <Footer />
     </div>
   );
 }
@@ -39,9 +58,14 @@ export async function getStaticProps() {
   const exploreData = await fetch("https://links.papareact.com/pyp").then(
     (res) => res.json()
   );
+
+  const cardsData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
+  );
   return {
     props: {
       exploreData: [...exploreData],
+      cardsData: [...cardsData],
     },
   };
 }
